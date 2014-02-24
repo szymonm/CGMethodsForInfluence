f4afc89b488c829edcc3ef75c86eec4ad9616a05
Szymon Matejczyk <smatejczyk@gmail.com>
2014-02-25 00:02:51 +0100

    topKFromMap function

--- src/main/scala/pl/szymonmatejczyk/competetiveShapley/common/package.scala
+++ src/main/scala/pl/szymonmatejczyk/competetiveShapley/common/package.scala
@@ -1,7 +1,15 @@
 package pl.szymonmatejczyk.competetiveShapley
 
+import scala.collection.mutable.PriorityQueue
+import scala.collection.immutable.SortedSet
+
 package object common {
   def doubleToLong(x: Double, denominator: Long) = (x * denominator).toLong
 
   def longToDouble(l: Long, denominator: Double) = l.toDouble / denominator
+  
+  def topKFromMap[K](k : Int, map : Map[K, Double]) : Seq[K] = {
+    (SortedSet[(K, Double)]()(Ordering.by[(K, Double), Double](-_._2)) ++ map).
+      take(k).map(_._1).toSeq
+  }
 }
