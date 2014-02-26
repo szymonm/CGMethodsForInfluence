46dab89dd3c0fdcaf2770b19787c5b07ab09465d
Szymon Matejczyk <smatejczyk@gmail.com>
2014-02-26 23:41:15 +0100

    R script to generate diagrams

---
+++ scripts/diagrams.R
@@ -0,0 +1,23 @@
+# Args: filename, name
+args <- commandArgs(trailingOnly=T)
+filename <- args[1]
+print(paste("Reading from:", filename))
+
+data <- read.table(args[1])
+
+plotDiagram <- function(name, table) {
+  png(paste(filename,".png", sep=""))
+  rows <- dim(data)[1]
+  plot(as.numeric(table[1,-1]), as.numeric(table[2, -1]), 
+       main=name, type="l", lty=1, 
+       xlab="initial seed size", ylab="influence on network")
+  for (i in 2:rows) {
+    lines(as.numeric(table[1,-1]), as.numeric(table[i,-1]), lty=i, col=i)
+  }
+  legend(data[1,2] + 1, max(table[2, -1]), data[,1], lty=1:rows, col=1:rows,
+         cex=0.8)
+  dev.off()
+}
+
+plotDiagram(args[2], data)
+
