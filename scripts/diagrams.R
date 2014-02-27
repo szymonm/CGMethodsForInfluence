a5c3887d25aa7fd0009f36c280c6c3edd36cc4e0
Szymon Matejczyk <smatejczyk@gmail.com>
2014-02-27 16:29:23 +0100

    Bug fixes

--- scripts/diagrams.R
+++ scripts/diagrams.R
@@ -7,14 +7,14 @@ data <- read.table(args[1])
 
 plotDiagram <- function(name, table) {
   png(paste(filename,".png", sep=""))
-  rows <- dim(data)[1]
+  rows <- dim(table)[1]
   plot(as.numeric(table[1,-1]), as.numeric(table[2, -1]), 
-       main=name, type="l", lty=1, 
+       main=name, type="l", lty=2, col=2, 
        xlab="initial seed size", ylab="influence on network")
-  for (i in 2:rows) {
+  for (i in 3:rows) {
     lines(as.numeric(table[1,-1]), as.numeric(table[i,-1]), lty=i, col=i)
   }
-  legend(data[1,2] + 1, max(table[2, -1]), data[,1], lty=1:rows, col=1:rows,
+  legend(data[1,2] + 1, max(table[2, -1]), data[-1,1], lty=2:rows, col=2:rows,
          cex=0.8)
   dev.off()
 }
