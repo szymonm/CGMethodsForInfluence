f4fb2d5c0f421d3885bc2215069b4d81b2db0e5c
Szymon Matejczyk <smatejczyk@gmail.com>
2014-02-27 20:02:52 +0100

    Game 5 fixes

--- src/main/scala/pl/szymonmatejczyk/competetiveShapley/michalakGames/InfluenceAboveThresholdGameSV.scala
+++ src/main/scala/pl/szymonmatejczyk/competetiveShapley/michalakGames/InfluenceAboveThresholdGameSV.scala
@@ -26,31 +26,44 @@ trait InfluenceAboveThresholdGameSV {
   }
   
   def betas() : M = alphas(x => x * x)
+
+  def pii(m: Int, inD: Int, node: Int, alpha: M, beta: M,
+    cutoff: M) = {
+    if (inD == 0)
+      1.0
+   else if (inD == 1)
+      0.5 + 0.5 * (1 - alpha(node.value)) // 1/2 + 1/2 * (1 - wij)  
+   else {
+      val mi = m.toDouble / (inD) * alpha(node)
+      val sigma = m.toDouble * (inD - m) / (inD * (inD - 1)) *
+        (beta(node) - alpha(node) / inD)
+      normalCDF(cutoff(node), mi, sigma)
+    }
+  }
   
   def computeSingleSV(node : Int, cutoff : M, alpha : M, beta : M) : Double = {
     var res = 0.0
     val inDegree = g.get(node).inDegree
     0.until(inDegree).foreach{
-      m => 
-        val mi = m.toDouble / (inDegree) * alpha(node)
-        val sigma = m.toDouble * (inDegree - m) / (inDegree * (inDegree - 1)) * 
-          (beta(node) - alpha(node)/inDegree)
-        val p = normalCDF(cutoff(node), mi, sigma)
-        res += p / (1.0 + inDegree)
+      m => res += pii(m, inDegree, node, alpha, beta, cutoff)/ (1.0 + inDegree)
     }
     g.get(node).inNeighbors.foreach {
       neighbour =>
         var p = 0.0
         0.to(neighbour.inDegree).foreach{
           m =>
-            val wij = g.get(node).findOutgoingTo(neighbour).fold(0.0)(_.weight)
-            val mi = m / (neighbour.inDegree - 1) * (alpha(neighbour) - wij)
-            val sigma = m * (neighbour.inDegree - 1 - m) / ((neighbour.inDegree - 1) * 
+            if (neighbour.inDegree <= 1) {
+              p += 1 / 2 * g.get(node).findOutgoingTo(neighbour).fold(0.0)(_.weight) // 1/2 * wij
+            } else {
+              val wij = g.get(node).findOutgoingTo(neighbour).fold(0.0)(_.weight)
+              val mi = m / (neighbour.inDegree - 1) * (alpha(neighbour) - wij)
+              val sigma = m * (neighbour.inDegree - 1 - m) / ((neighbour.inDegree - 1) *
                 (neighbour.inDegree - 2)) * (beta(neighbour) - wij * wij) -
                 (alpha(neighbour) - wij * wij) / (neighbour.inDegree - 1)
-            val z = 0.5 * (erf((cutoff(neighbour) - mi)/ (math.sqrt(2) * sigma)) - 
+              val z = 0.5 * (erf((cutoff(neighbour) - mi) / (math.sqrt(2) * sigma)) -
                 erf((cutoff(neighbour) - wij - m) / math.sqrt(2) * sigma))
-            p += z * (neighbour.inDegree - m) / (neighbour.inDegree * (neighbour.inDegree + 1))
+              p += z * (neighbour.inDegree - m) / (neighbour.inDegree * (neighbour.inDegree + 1))
+            }
         }
         res += p        
     }
