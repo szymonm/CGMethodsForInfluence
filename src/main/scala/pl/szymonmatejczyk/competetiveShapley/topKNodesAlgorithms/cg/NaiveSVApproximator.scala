f7838c8984eee0c92510f3dd5cd5fd0f68b2704d
Szymon Matejczyk <smatejczyk@gmail.com>
2014-03-01 11:41:51 +0100

    buffered SV approximator

--- src/main/scala/pl/szymonmatejczyk/competetiveShapley/topKNodesAlgorithms/cg/NaiveSVApproximator.scala
+++ src/main/scala/pl/szymonmatejczyk/competetiveShapley/topKNodesAlgorithms/cg/NaiveSVApproximator.scala
@@ -1,18 +1,25 @@
 package pl.szymonmatejczyk.competetiveShapley.topKNodesAlgorithms.cg
 
 import scala.collection._
+import collection.JavaConversions._
 import pl.szymonmatejczyk.competetiveShapley.utils.GeneratorOverIterator
+import java.util.concurrent.atomic.AtomicInteger
 
 trait NaiveSVApproximator {
-  def approximate[T](generator: () => Option[Seq[T]], calculator: Seq[T] => Map[T, Double]): Map[T, Double] = {
-    val res = mutable.Map[T, Double]()
-    var counter = 0
-    new GeneratorOverIterator(generator).foreach {
-      counter += 1
-      seq => calculator(seq).foreach {
-        case (t, value) => res.update(t, res.getOrElse(t, 0.0) + value)
+  def approximate[T](generator: () => Option[Seq[T]], calculator: Seq[T] => Map[T, Double],
+      chunkSize : Int = 1): Map[T, Double] = {
+    val res = new java.util.concurrent.ConcurrentHashMap[T, Double]()
+    var counter = new AtomicInteger(0)
+    new GeneratorOverIterator(generator).grouped(chunkSize).foreach {
+      _.par.foreach {
+        seq =>
+          counter.incrementAndGet()
+          calculator(seq).foreach {
+            case (t, value) => res.update(t, res.getOrElse(t, 0.0) + value)
+          }
       }
     }
-    res.mapValues(_ / counter)
+    res.mapValues(_ / counter.get())
   }
+
 }
