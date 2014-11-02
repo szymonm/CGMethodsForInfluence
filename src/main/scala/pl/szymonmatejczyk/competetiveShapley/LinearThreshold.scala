9c594c46a8690f818d7046e04fa6dacc16ef8c1f
Szymon Matejczyk <smatejczyk@gmail.com>
2014-11-02 12:54:14 +0100

    new LinearThreshold model impl

--- src/main/scala/pl/szymonmatejczyk/competetiveShapley/LinearThreshold.scala
+++ src/main/scala/pl/szymonmatejczyk/competetiveShapley/LinearThreshold.scala
@@ -51,31 +51,6 @@ trait LinearThreshold {
       activationThresholdGenerator : (() => Double) = uniformActivationThresholdGenerator) : Int = 
         incrementalRandomLinearThresholdSeedQuality(Seq(seed.toSet), activationThresholdGenerator).head
 
-//    val activationThresholdsLeft = new Array[Double](self.maxNodeId + 1)
-//    graph.nodes.foreach(n => activationThresholdsLeft(n.value) = activationThresholdGenerator())
-//      
-//    @tailrec
-//    def activateNodes(activated: ParSet[Int], lastPhaseActivated: ParSet[Int]): ParSet[Int] = {
-//      if (lastPhaseActivated.isEmpty)
-//        activated
-//      else {
-//        val thresholdsFilled: ParSet[Int] = lastPhaseActivated.toSeq
-//          .flatMap(node => 
-//            g.get(node).outgoing
-//              .filterNot(e => activated.contains(e._2))
-//              .map(e => (e._2, e.weight.toDouble / weightDenominator)))
-//          .groupBy(_._1)
-//          .map{ case (node, listOfInfluences) => (node, listOfInfluences.map(_._2).sum)}
-//          .flatMap{ case (node, sumOfInfluence) =>
-//            activationThresholdsLeft(node) -= sumOfInfluence
-//            if (activationThresholdsLeft(node) <= 0) ParSet[Int](node) else ParSet[Int]()}.toSet
-//        
-//        activateNodes(activated ++ thresholdsFilled, thresholdsFilled)
-//      } 
-//    }
-//    activateNodes(ParSet() ++ seed, ParSet() ++ seed).size
-//  }
-  
   def mcLinearThresholdSeedQuality(seed : Seq[Int], runs : Int, 
       atg : (() => Double) = uniformActivationThresholdGenerator) : Double = {
     1.to(runs).par.map{_ => randomLinearThresholdSeedQuality(seed, atg).toDouble}.sum / runs
